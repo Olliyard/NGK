fb63d8bbf015371b12e8aba64622b28f10fda0d8
ase <revilo624@gmail.com>
2022-03-07 12:45:46 +0100

    client change

--- Oevelse6/client/client.cpp
+++ Oevelse6/client/client.cpp
@@ -12,13 +12,18 @@
 #define SIZE 1000
 using namespace std;
 
+void receiveFile(string fileName, int sockfd)
+{
+
+}
+
 int main(int argc, char *argv[])
 {
     int buffersize = 0, recv_size = 0, size = 0, r_size, w_size;
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    char buffer[SIZE];
+    char buffer[256], fbuf[SIZE];
 
     if (argc < 3) {
        error("ERROR no hostname/port");
@@ -44,19 +49,45 @@ int main(int argc, char *argv[])
         error("ERROR connecting");
     }
     
-    //reads text over TCP. Message is stored in 'file'
-    if(readTextTCP(buffer, SIZE, sockfd) <0){
+    //reads text over TCP. Message is stored in 'buffer'
+    bzero(buffer,256);
+    if(readTextTCP(buffer, sizeof(buffer)-1, sockfd) <0){
         error("ERROR on reading");
     }
     cout << buffer << endl;
-    
-    cout << "Please enter the message: ";
+    cout << "Please enter file destination/filename: ";
     
     //write to server
-    bzero(buffer,SIZE);
-    fgets(buffer,SIZE,stdin);
+    bzero(buffer,256);
+    fgets(buffer,256,stdin);
     writeTextTCP(sockfd, buffer);
 
+    buffer[strcspn(buffer, "\n")] = 0;
+    //handle data transfer. Use ofstream (file output) to extract filename from buffer.
+    ofstream outfile(extractFileName(buffer), std::ios::binary);
+
+    cout << "Getting file size..." << endl;
+
+    bzero(buffer,256);
+    readTextTCP(buffer, sizeof(buffer), sockfd);
+    cout << "Buf: " <<  buffer << endl;
+    sscanf(buffer, "%d", &size);
+    cout << "File size (int): " << buffer << endl;
+
+    cout << "Getting file..." << endl;
+    while(size>=0){
+        n = read(sockfd, fbuf, (size > 1000 ? 1000 : size));
+        if(n <= 0){
+            error("ERROR reading from socket");
+        }
+
+        outfile.write((char*)fbuf, 1000);
+        size-=1000;
+
+        cout << "Size left: " << size << "  Sending size: " << n << endl;
+    }
+    cout << "File transfer complete!" << endl;
+    outfile.close();
     close(sockfd);
     return 0;
 }
